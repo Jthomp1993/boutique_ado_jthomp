22f10df6072673175988028c7b1d81274efe3420
Jthomp1993 <joshuagt93@hotmail.com>
2021-03-20 14:51:14 +0000

    Added basic payment functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,6 +1,6 @@
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
@@ -34,4 +34,39 @@ card.addEventListener('change', function (event) {
     } else {
         errorDiv.textContent = '';
     }
+});
+
+// Handle form submit 
+
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+  ev.preventDefault();
+  // If the client secret was rendered server-side as a data-secret attribute
+  // on the <form> element, you can retrieve it here by calling `form.dataset.secret`
+  card.update({'disabled': true});
+  $('#submit-button').attr('disabled', true);
+  stripe.confirmCardPayment(clientSecret, {
+    payment_method: {
+      card: card,
+    }
+  }).then(function(result) {
+    if (result.error) {
+        var errorDiv = document.getElementById('card-errors');
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+        card.update({'disabled': false});
+        $('#submit-button').attr('disabled', false);
+    } else {
+      // The payment has been processed!
+      if (result.paymentIntent.status === 'succeeded') {
+        form.submit();
+      }
+    }
+  });
 });
