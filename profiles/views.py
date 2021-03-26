dbafe06033644a8ccfb2ef326cb3418ea115de71
Jthomp1993 <joshuagt93@hotmail.com>
2021-03-26 12:55:36 +0000

    Securing the views

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 from checkout.models import Order
@@ -8,6 +9,7 @@ from .forms import UserProfileForm
 # Create your views here.
 
 
+@login_required
 def profile(request):
     """ Render the user's profile """
     profile = get_object_or_404(UserProfile, user=request.user)
